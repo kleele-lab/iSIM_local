96f2d701840cbaab68141bbaf4ba31e588cbb6c4
nmarounina-eth <nmarounina@ethz.ch>
2024-12-17 15:32:32 +0100

    Implemented and tested the data generator

--- deconvolution/cuda_decon.py
+++ deconvolution/cuda_decon.py
@@ -9,10 +9,10 @@ from typing import Union
 import os
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+
 # os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = 'platform'
-##############
-#Keep this for information purposes : psf parameters are hard-encoded here
-##############
+
 @dataclass
 class Params():
     """ Class for storing parameters to be used in deconvolution"""
@@ -35,10 +35,10 @@ def make_kernel(image: np.ndarray, sigma=1.67, z_step=0.2):
     if image.ndim == 3:
         z_sigma = 0.48 / z_step
         sigma = [z_sigma, sigma, sigma]
-        print("3D images, sigma: ", sigma)
+        #print("3D images, sigma: ", sigma)
 
     size = image.shape
-    #size = [min([17, x]) for x in size]
+    # size = [min([17, x]) for x in size]
 
     # If even size dimensions crop to have a center pixel
     kernel = {'kernel_array': np.zeros(size, dtype=float),
@@ -51,23 +51,36 @@ def make_kernel(image: np.ndarray, sigma=1.67, z_step=0.2):
     return kernel
 
 
-def decon_ome_stack(file_dir, params=None):
+def get_data_c(data_t, size_c, size_z):
+    for channel in range(size_c):
+        data_c = data_t[:, channel, :, :]
+        if size_z == 1:
+            data_c = data_c[0, :, :]
+
+        yield channel, data_c
+
+
+def decon_ome_stack(file_dir, params):
     data = None
-    params = Params()
-    with tifffile.TiffFile(file_dir) as tif:# , is_mmstack=False, is_ome=True
+    params = params
+    with tifffile.TiffFile(file_dir) as tif:
         assert tif.is_imagej
         imagej_metadata = tif.imagej_metadata
-
 
         my_dict = xmltodict.parse(tif.ome_metadata, force_list={'Plane'})
         size_t = int(my_dict['OME']['Image']["Pixels"]["@SizeT"])
         size_z = int(my_dict['OME']['Image']["Pixels"]["@SizeZ"])
         size_c = int(my_dict['OME']['Image']["Pixels"]["@SizeC"])
+
         try:
             z_step = float(my_dict['OME']['Image']["Pixels"]['@PhysicalSizeZ'])
+            params = Params(z_step=z_step)
         except KeyError:
             print("Could not get z step size. Will put default 0.2")
             z_step = 0.2
+            params = Params()
+
+        assert params != None
         # 'XYCZT' or 'XYZCT' ?
         dim_order = my_dict['OME']['Image']["Pixels"]["@DimensionOrder"]
 
@@ -84,7 +97,7 @@ def decon_ome_stack(file_dir, params=None):
     #     size_c = 1
     #
     #     z_step = 0.2
-    print("\n Sizes : ", size_t, size_z, size_c)
+    print("\nSizes, t, z and c : ", size_t, size_z, size_c)
     print("Dim_order: ", dim_order)
 
     ndim = 2 if size_z == 1 else 3
@@ -105,7 +118,7 @@ def decon_ome_stack(file_dir, params=None):
 
     if size_t != data.shape[0]:
         size_t = data.shape[0]
-    print("SHAPE", data.shape)
+    print("Shape of data", data.shape)
 
     # Make data odd shaped
     original_size_data = data.shape
@@ -123,37 +136,31 @@ def decon_ome_stack(file_dir, params=None):
             crop[dim][1] = data.shape[dim]
     data = data[:, :, :, :crop[3][1], :crop[4][1]]
 
-    kernel_shape = data.shape[-2:] if ndim == 2 else [np.min([17, size_z]), *data.shape[-2:]]
-    # Decon
-    if params is None:
-        background = 100
-    else:
-        background = params.background
+    # kernel_shape = data.shape[-2:] if ndim == 2 else [np.min([17, size_z]), *data.shape[-2:]]
+    # # Decon
+    # if params is None:
+    #     background = 100
+    # else:
+    #     background = params.background
 
     my_slices = None
     decon = np.empty_like(data)
     for timepoint in tqdm(range(size_t)):
         data_t = data[timepoint, :, :, :, :]
-        for channel in range(size_c):
-            data_c = data_t[:, channel, :, :]
-            if size_z == 1:
-                data_c = data_c[0, :, :]
+        data_c_iterable = get_data_c(data_t, size_c, size_z)
+        # if my_slices is None:
+        # if ndim == 3:
+        for channel, data_c in data_c_iterable:
+            params.kernel = make_kernel(image=data_c, sigma=params.sigma, z_step=params.z_step)
+            # for z_i in tqdm(range(0, data_c.shape[0])):
+            # for ij in range(0, data_c.shape[0]):
+            maxval_slice = 65535 #16-bit images     np.max(data_c)
 
-            if my_slices is None:
-                # if ndim == 3:
+            result = restoration.richardson_lucy(data_c / maxval_slice,
+                                                 psf=params.kernel['kernel_array'],
+                                                 num_iter=10)  #
 
-                # padding = (data_c.shape[0] - params.kernel['kernel'].shape[0]) // 2
-                # params.kernel['kernel'] = np.pad(params.kernel['kernel'], ((padding, padding), (0, 0), (0, 0)))
-                params.kernel = make_kernel(image=data_c[0, :, :], sigma=params.sigma, z_step=params.z_step)
-                for z_i in tqdm( range(0, data_c.shape[0]) ):
-                #for ij in range(0, data_c.shape[0]):
-                    maxval_slice = np.max(data_c[z_i, :, :])
-                    result = restoration.richardson_lucy(data_c[z_i, :, :] / maxval_slice,
-                                                         psf=params.kernel['kernel_array'],
-                                                         num_iter=10)  #
-
-                    decon[timepoint, z_i, channel, :, :] = result * maxval_slice
-
+            decon[timepoint, :, channel, :, :] = result * maxval_slice
 
     # Crop the data back if we padded it
     if original_size_data != decon.shape:
@@ -162,39 +169,8 @@ def decon_ome_stack(file_dir, params=None):
         decon = np.pad(decon, pad)
     print("DECON SHAPE ", decon.shape)
 
-
-
-    # Swap axes back
-    # print("DECON", decon.shape)
-    # if size_z > 1 and size_c > 1 and dim_order == 'XYZCT':
-    #     decon = np.moveaxis(decon, 1, 2)
-    # decon = np.squeeze(decon)
-    # print("DECON READY", decon.shape)
-    # Output
-
-    # Adjust metadata
-    #    try:
-    #        imagej_metadata['min'] = np.min(decon)
-    #        imagej_metadata['max'] = np.max(decon)
-    #        imagej_metadata['Ranges'] = (np.min(decon), np.max(decon))
-    #        for idx, lut in enumerate(imagej_metadata['LUTs']):
-    #            imagej_metadata['LUTs'][idx] = imagej_metadata['LUTs'][idx].tolist()
-    #    except TypeError:
-    #        print("Could not set imagej_metadata")
-    # info = json.loads(imagej_metadata['Info'])
-    # Construct the correct one here
-    # info['AxisOrder'] = ['position', 'time', 'z', 'channel']
-    # imagej_metadata['Info'] = json.dumps(info)
-
     out_file = os.path.basename(file_dir).rsplit('.', 2)
     out_file_tiff = out_file[0] + ".".join(["_decon", *out_file[1:]])
-
-    # filename = str(file_dir.split('.ome.tif')[0]) + '_metadata.txt'
-    # f_metadata = open(filename, 'r')
-    # # metadata_text = f_metadata.read()
-    # metadata_json = json.load(f_metadata)
-    #
-    # f_metadata.close()
 
     with tifffile.TiffWriter(os.path.join(os.path.dirname(file_dir), out_file_tiff), imagej=True) as dst:
         for decon_one in decon:
@@ -204,5 +180,3 @@ def decon_ome_stack(file_dir, params=None):
                 contiguous=True,
                 metadata=imagej_metadata,
             )
-
-    #io.imsave(os.path.join(os.path.dirname(file_dir), out_file_tiff), decon, metadata=imagej_metadata)
