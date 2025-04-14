7ac655df3b2b6cc0c1a5a2fd0c9fcbe849bdb341
nmarounina-eth <nmarounina@ethz.ch>
2025-04-14 15:40:59 +0200

    Created a generic Image class

--- deconvolution/cuda_decon.py
+++ deconvolution/cuda_decon.py
@@ -1,3 +1,5 @@
+import posixpath
+
 from skimage import restoration
 from scipy import ndimage
 import numpy as np
@@ -11,6 +13,7 @@ import javabridge as jb
 import bioformats as bf
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 
 # os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = 'platform'
 def _init_logger():
@@ -37,18 +40,132 @@ def _init_logger():
             logLevel)
 
 
-jb.start_vm(class_path=bf.JARS)
-logger=_init_logger()
-
-
 @dataclass
 class Params():
     """ Class for storing parameters to be used in deconvolution"""
     sigma: float = 3.9 / 2.335
-    z_step: float = -1.  # default value designated later
+    z_step: float = 0.2
     background: Union[int, str] = 'median'
     kernel = None
 
+
+class Image():
+    """ Generic class to store the data for the deconvolution 
+    Image can be read from tiff or vsi"""
+    def __init__(self):
+        data: np.ndarray = np.empty(1)
+        metadata: dict = {}
+        z_step: float = 0.2
+        size_t: int = 0
+        size_c: int = 0
+        size_z: int = 0
+        out_file_end: str = ''
+
+    def read_tiff(self, file_dir: posixpath) -> None:
+        tif = tifffile.TiffFile(file_dir)
+        Image.metadata = tif.imagej_metadata
+
+        my_dict = xmltodict.parse(tif.ome_metadata, force_list={'Plane'})
+        size_t = int(my_dict['OME']['Image']["Pixels"]["@SizeT"])
+        size_z = int(my_dict['OME']['Image']["Pixels"]["@SizeZ"])
+        size_c = int(my_dict['OME']['Image']["Pixels"]["@SizeC"])
+
+        try:
+            Image.z_step = float(my_dict['OME']['Image']["Pixels"]['@PhysicalSizeZ'])
+        except KeyError:
+            print("Could not get z step size. Will put default 0.2")
+            Image.z_step = 0.2
+
+        # 'XYCZT' or 'XYZCT' ?
+        dim_order = my_dict['OME']['Image']["Pixels"]["@DimensionOrder"]
+        Image.data = tif.asarray()
+
+        # This is legacy code to handle tif dimension issues
+
+        # if data is None:
+        #     print("ATTENTION: NORMAL READING OF TIFF FAILED! RESORT TO BASIC! ASSUME 1 TIME POINT & 1 CHANNEL!")
+        #     data = io.imread(file_dir, plugin='pil')
+        #
+        #     dim_order = 'XYCZT'
+        #
+        #     size_t = 1
+        #     size_z = data.shape[0]
+        #     size_c = 1
+        #
+        #     z_step = 0.2
+        print("\nReading tif:")
+        print("\nSizes, t, z, and c : ", size_t, size_z, size_c)
+        print("Dim_order: ", dim_order)
+
+        ndim = 2 if size_z == 1 else 3
+        # Make standardized array with all dimensions
+        # time [0], z[1], c[2] if not there, just missing depending on order
+        # If it was recorded differently, swap dimensions
+
+        if size_t == 1:
+            Image.data = np.expand_dims(Image.data, 0)
+
+        if size_z > 1 and size_c > 1 and dim_order == 'XYZCT':
+            Image.data = np.moveaxis(Image.data, 1, 2)
+
+        if size_z == 1 and len(Image.data.shape) < 5:
+            Image.data = np.expand_dims(Image.data, 1)
+        if size_c == 1 and len(Image.data.shape) < 5:
+            Image.data = np.expand_dims(Image.data, 2)
+
+        if size_t != Image.data.shape[0]:
+            size_t = Image.data.shape[0]
+
+        Image.size_z = size_z
+        Image.size_t = size_t
+        Image.size_c = size_c
+        print("New shape of data", Image.data.shape, "\n")
+
+        out_file = os.path.basename(file_dir).rsplit('.', 2)
+        Image.out_file_end = out_file[0] + ".".join(["_decon", *out_file[1:]])
+
+    def read_vsi(self, file_dir: posixpath) -> None:
+        jb.start_vm(class_path=bf.JARS)
+        logger = _init_logger()
+
+        vsi = bf.ImageReader(file_dir)
+        txt_metadata = bf.get_omexml_metadata(file_dir)
+        txt_metadata = txt_metadata.encode('ascii', errors="ignore")
+
+        my_dict = xmltodict.parse(txt_metadata, force_list={'Plane'})
+        Image.metadata = my_dict
+
+        size_t = int(my_dict['OME']['Image'][0]['Pixels']['@SizeT'])
+        size_z = int(my_dict['OME']['Image'][0]['Pixels']['@SizeZ'])
+        size_c = int(my_dict['OME']['Image'][0]['Pixels']['@SizeC'])
+        size_x = int(my_dict['OME']['Image'][0]['Pixels']['@SizeX'])
+        size_y = int(my_dict['OME']['Image'][0]['Pixels']['@SizeY'])
+
+        dim_order = my_dict['OME']['Image'][0]['Pixels']['@DimensionOrder']
+
+        try :
+            Image.z_step = float(my_dict['OME']['Image'][0]["Pixels"]['@PhysicalSizeZ'])
+        except KeyError:
+            print("Could not get z step size. Will put default 0.2")
+            Image.z_step = 0.2
+
+        if dim_order == 'XYCZT':
+            # data = np.zeros((size_t, size_z, size_c, size_y, size_x))
+            data = np.zeros((size_x, size_y, size_c, size_z, size_t))
+            for i_z in range(0, size_z):
+                for i_t in range(0, size_t):
+                    data[:, :, :, i_z, i_t] = vsi.read(series='0', z=i_z, t=i_t)
+        else:
+            raise "Dim order is NOT XYZCT"
+
+        Image.data = np.moveaxis(data, [0, 1, 2, 3, 4], [4, 3, 2, 1, 0])
+        Image.size_z = size_z
+        Image.size_t = size_t
+        Image.size_c = size_c
+
+        out_file = os.path.basename(file_dir).rsplit('.', 2)
+        Image.out_file_end = out_file[0] + ".".join(["_decon.tif "])
+        jb.kill_vm()
 
 def make_kernel(image: np.ndarray, sigma=1.67, z_step=0.2):
     """
@@ -101,119 +218,38 @@ def decon_ome_stack(file_dir, background):
 
     """
 
-    data = None
     params = Params
     params.background = background
 
+    Img = Image()
     if file_dir.split('.')[-1] == "tif":
-        tif = tifffile.TiffFile(file_dir)
-        # assert tif.is_imagej
-        image_metadata = tif.imagej_metadata
-
-        my_dict = xmltodict.parse(tif.ome_metadata, force_list={'Plane'})
-        size_t = int(my_dict['OME']['Image']["Pixels"]["@SizeT"])
-        size_z = int(my_dict['OME']['Image']["Pixels"]["@SizeZ"])
-        size_c = int(my_dict['OME']['Image']["Pixels"]["@SizeC"])
-
-        try:
-            z_step = float(my_dict['OME']['Image']["Pixels"]['@PhysicalSizeZ'])
-            params.z_step = z_step  # = Params(z_step=z_step)
-        except KeyError:
-            print("Could not get z step size. Will put default 0.2")
-            params.z_step = 0.2
-            # params = Params()
-
-        assert params != None
-        # 'XYCZT' or 'XYZCT' ?
-        dim_order = my_dict['OME']['Image']["Pixels"]["@DimensionOrder"]
-
-        data = tif.asarray()
+        Img.read_tiff(file_dir)
 
     elif file_dir.split('.')[-1] == "vsi":
-        vsi = bf.ImageReader(file_dir)
-        image_metadata = bf.get_omexml_metadata(file_dir)
-        image_metadata = image_metadata.encode('ascii', errors="ignore")
-
-        my_dict = xmltodict.parse(image_metadata, force_list={'Plane'})
-        image_metadata = my_dict
-
-        size_t = int(my_dict['OME']['Image'][0]['Pixels']['@SizeT'])
-        size_z = int(my_dict['OME']['Image'][0]['Pixels']['@SizeZ'])
-        size_c = int(my_dict['OME']['Image'][0]['Pixels']['@SizeC'])
-        size_x = int(my_dict['OME']['Image'][0]['Pixels']['@SizeX'])
-        size_y = int(my_dict['OME']['Image'][0]['Pixels']['@SizeY'])
-
-        dim_order = my_dict['OME']['Image'][0]['Pixels']['@DimensionOrder']
-
-        if dim_order == 'XYCZT':
-            # data = np.zeros((size_t, size_z, size_c, size_y, size_x))
-            data = np.zeros((size_x, size_y, size_c, size_z, size_t))
-            for i_z in range(0, size_z):
-                for i_t in range(0, size_t):
-                    data[:, :, :, i_z, i_t] = vsi.read(series='0', z=i_z, t=i_t)
-        else:
-            raise "Dim order is NOT XYZCT"
-
-        data = np.moveaxis(data, [0, 1, 2, 3, 4], [4, 3, 2, 1, 0])
-
-        jb.kill_vm()
-
-    # if data is None:
-    #     print("ATTENTION: NORMAL READING OF TIFF FAILED! RESORT TO BASIC! ASSUME 1 TIME POINT & 1 CHANNEL!")
-    #     data = io.imread(file_dir, plugin='pil')
-    #
-    #     dim_order = 'XYCZT'
-    #
-    #     size_t = 1
-    #     size_z = data.shape[0]
-    #     size_c = 1
-    #
-    #     z_step = 0.2
-    print("\nSizes, t, z, and c : ", size_t, size_z, size_c)
-    print("Dim_order: ", dim_order)
-
-    ndim = 2 if size_z == 1 else 3
-    # Make standardized array with all dimensions
-    # time [0], z[1], c[2] if not there, just missing depending on order
-    # If it was recorded differently, swap dimensions
-
-    if size_t == 1:
-        data = np.expand_dims(data, 0)
-
-    if size_z > 1 and size_c > 1 and dim_order == 'XYZCT':
-        data = np.moveaxis(data, 1, 2)
-
-    if size_z == 1 and len(data.shape) < 5:
-        data = np.expand_dims(data, 1)
-    if size_c == 1 and len(data.shape) < 5:
-        data = np.expand_dims(data, 2)
-
-    if size_t != data.shape[0]:
-        size_t = data.shape[0]
-    print("Shape of data", data.shape, "\n")
+        Img.read_vsi(file_dir)
 
     # Make data odd shaped
-    original_size_data = data.shape
+    original_size_data = Img.data.shape
     pad = tuple(np.zeros((5, 2), int))
     crop = tuple(np.zeros((5, 2), int))
 
-    if data.shape[1] % 2 == 0:
+    if Img.data.shape[1] % 2 == 0:
         pad_here = tuple(np.zeros((5, 2), int))
         pad_here[1][1] = 1
-        data = np.pad(data, pad)
+        Img.data = np.pad(Img.data, pad)
     for dim in [3, 4]:
-        if data.shape[dim] % 2 == 0:
+        if Img.data.shape[dim] % 2 == 0:
             pad[dim][1] = 1
-            crop[dim][1] = data.shape[dim] - 1
+            crop[dim][1] = Img.data.shape[dim] - 1
         else:
-            crop[dim][1] = data.shape[dim]
-    data = data[:, :, :, :crop[3][1], :crop[4][1]]
+            crop[dim][1] = Img.data.shape[dim]
+    Img.data = Img.data[:, :, :, :crop[3][1], :crop[4][1]]
 
     # start of the deconvolution loop
-    decon = np.empty_like(data)
-    for timepoint in tqdm(range(size_t)):
-        data_t = data[timepoint, :, :, :, :]
-        data_c_iterable = get_data_c(data_t, size_c, size_z)
+    decon = np.empty_like(Img.data)
+    for timepoint in tqdm(range(Img.size_t)):
+        data_t = Img.data[timepoint, :, :, :, :]
+        data_c_iterable = get_data_c(data_t, Img.size_c, Img.size_z)
 
         for channel, data_c in data_c_iterable:
             params.kernel = make_kernel(image=data_c, sigma=params.sigma, z_step=params.z_step)
@@ -257,18 +293,12 @@ def decon_ome_stack(file_dir, background):
 
     print("DECON SHAPE ", decon.shape)
 
-    out_file = os.path.basename(file_dir).rsplit('.', 2)
-    out_file_tiff = out_file[0] + ".".join(["_decon", *out_file[1:]])
-
-    if out_file[-1] == 'vsi':
-        out_file_tiff = out_file[0]+'_decon.tif'
-
-    with tifffile.TiffWriter(os.path.join(os.path.dirname(file_dir), out_file_tiff), imagej=True) as dst:
+    with tifffile.TiffWriter(os.path.join(os.path.dirname(file_dir), Img.out_file_end), imagej=True) as dst:
         for decon_one in decon:
             frame = decon_one
             dst.write(
                 frame,
                 contiguous=True,
-                metadata=image_metadata,
+                metadata=Img.metadata,
                 dtype=np.uint16
             )
