1e871077924be78b826cf1380b77244d8d2efdd2
nmarounina-eth <nmarounina@eu-lo-g3-046.euler.ethz.ch>
2025-09-25 12:03:21 +0200

    Slicing of the z-stack

--- deconvolution/cuda_decon.py
+++ deconvolution/cuda_decon.py
@@ -11,9 +11,20 @@ from typing import Union
 import os
 import javabridge as jb
 import bioformats as bf
+import subprocess as sp
+import os
+
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
+def get_gpu_memory():
+    command = "nvidia-smi --query-gpu=memory.free --format=csv"
+    memory_free_info = sp.check_output(command.split()).decode('ascii').split('\n')[:-1][1:]
+    memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
+    return memory_free_values
+
+
+GPUMEM = get_gpu_memory()
 
 # os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = 'platform'
 def _init_logger():
@@ -186,14 +197,21 @@ class Image():
             print("Could not get z step size. Will put default 0.2")
             Image.z_step = 0.2
 
+        print("1")
         if dim_order == 'XYCZT':
-            # data = np.zeros((size_t, size_z, size_c, size_y, size_x))
+            print(size_x, size_y, size_c, size_z, size_t)
             data = np.zeros((size_x, size_y, size_c, size_z, size_t))
             for i_z in range(0, size_z):
                 for i_t in range(0, size_t):
-                    data[:, :, :, i_z, i_t] = vsi.read(series='0', z=i_z, t=i_t)
+                    interm_data=vsi.read(series='0', z=i_z, t=i_t)
+                    if len(interm_data.shape) == 2:
+                        interm_data=np.expand_dims(interm_data, axis=-1)
+                        data[:, :, :, i_z, i_t] = interm_data
+                    else :
+                        data[:, :, :, i_z, i_t] = interm_data
         else:
             raise "Dim order is NOT XYZCT"
+        print("2")
 
         #flowdec needs a re-scale of the vsi data (vsi comes with values between 0 and 1)
         #here the re-scale is done up to the uint16 values
@@ -257,6 +275,31 @@ def get_data_c(data_t, size_c, size_z):
         yield channel, data_c
 
 
+def run_deconvolution(data_c, params) :
+    """
+    Feeds the deconvolution function z-stacks that will not 
+    overflow the GPU memory
+    """
+    
+
+    EMPIRICAL_LIM_10G=2300*2300*7 #pixels 
+    CURRENT_GPUMEM_GB=GPUMEM[0]/953.7 #conversion from mebibytes to GB
+
+    
+    #Given the current z-stack, how many slices do we need for this GPU model
+    #so we can run the decon and do not saturate the VRAM ?
+    ALLOWED_NB_PX=EMPIRICAL_LIM_10G*CURRENT_GPUMEM_GB/10.
+
+    #How many z-slices fit in the allowed nb of px ?
+
+    NZ,NY,NX=data_c.shape
+    Nslice=NX*NY
+    NZ_per_cycle=ALLOWED_NB_PX//Nslice
+    
+    
+    #richardson_lucy(data_c, params=params)
+    return NX
+
 def decon_ome_stack(file_dir, background):
     """
     Main deconvolution routine that reads in images, processes,
@@ -265,7 +308,8 @@ def decon_ome_stack(file_dir, background):
     """
 
     params = Params
-    params.background = background
+    params.background = background 
+
 
     Img = Image()
     if file_dir.split('.')[-1] == "tif":
@@ -293,15 +337,18 @@ def decon_ome_stack(file_dir, background):
 
     # start of the deconvolution loop
     decon = np.empty_like(Img.data)
+    #print('3')
 
     for timepoint in tqdm(range(Img.size_t)):
+
         data_t = Img.data[timepoint, :, :, :, :]
         data_c_iterable = get_data_c(data_t, Img.size_c, Img.size_z)
         for channel, data_c in data_c_iterable:
+
             params.kernel = make_kernel(image=data_c, sigma=params.sigma, z_step=params.z_step)
 
-            decon[timepoint, :, channel, :, :] = richardson_lucy(data_c, params=params)
-
+            #decon[timepoint, :, channel, :, :] = run_deconvolution() richardson_lucy(data_c, params=params)
+            A=run_deconvolution( data_c, params)
 
     # Crop the data back if we padded it
     #if original_size_data != decon.shape:
